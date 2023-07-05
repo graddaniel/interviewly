47462b99dea4a23d5036302e29a023e9c7fa374a
Daniel Grad <graddaniel0@gmail.com>
2023-07-05 22:20:29 +0200

    [shared] Created shared library

---
+++ shared/index.ts
@@ -0,0 +1,8 @@
+import countries from './definitions/countries';
+export { countries };
+
+import * as AccountTypes from './types/account';
+export { AccountTypes };
+
+import * as ProfileTypes from './types/profile';
+export { ProfileTypes };
