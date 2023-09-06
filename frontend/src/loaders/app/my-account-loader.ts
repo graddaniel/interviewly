8fb1caf6f62c2946aa218360c557523b34877c30
Daniel Grad <graddaniel0@gmail.com>
2023-09-06 07:27:44 +0200

    [frontend] Fixed myAccount page loader

--- frontend/src/loaders/app/my-account-loader.ts
+++ frontend/src/loaders/app/my-account-loader.ts
@@ -13,25 +13,35 @@ export default async function MyAccountLoader({
         return null;
     }
 
-    const upcomingMeeting = await MeetingService.getAllMeetings({
-        sort: 'date',
-        limit: 1,
-    });
+    try {
+        const upcomingMeeting = await MeetingService.getAllMeetings({
+            sort: 'date',
+            limit: 1,
+        });
+    
+        const profile = await ProfileService.getProfile();
+    
+        const myAccountData: any = {
+            upcomingMeeting: upcomingMeeting.length > 0 ? upcomingMeeting[0] : {},
+            profile,
+        };
+    
+        if (auth.type === AccountTypes.Type.RECRUITER) {
+            const teamMembers = await CompanyService.getCompanyAccounts();
+            myAccountData.teamMembers = teamMembers.slice(0, 2);
+    
+            const projects = await ProjectService.getProjects();
+            myAccountData.projects = projects.slice(0, 3);
+        }
 
-    const profile = await ProfileService.getProfile();
-
-    const myAccountData: any = {
-        upcomingMeeting: upcomingMeeting.length > 0 ? upcomingMeeting[0] : {},
-        profile,
-    };
-
-    if (auth.type === AccountTypes.Type.RECRUITER) {
-        const teamMembers = await CompanyService.getCompanyAccounts();
-        myAccountData.teamMembers = teamMembers.slice(0, 2);
-
-        const projects = await ProjectService.getProjects();
-        myAccountData.projects = projects.slice(0, 3);
+        return {
+            success: true,
+            data: myAccountData,
+        };
+    } catch (error) {
+        return {
+            success: false,
+            error,
+        };
     }
-
-    return myAccountData;
 }
