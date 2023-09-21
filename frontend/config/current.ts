2602b15c63fca37d7e4e5b7fc3b56657c11795a8
Daniel Grad <graddaniel0@gmail.com>
2023-09-21 08:08:36 +0200

    staging config

--- frontend/config/current.ts
+++ frontend/config/current.ts
@@ -1,5 +1,5 @@
-const API_HOST = 'http://localhost:7081';
-const JANUS_HOST = 'http://localhost:8088/janus';
+const API_HOST = 'https://id8d03szbk.execute-api.eu-central-1.amazonaws.com/prod/api';
+const JANUS_HOST = 'https://id8d03szbk.execute-api.eu-central-1.amazonaws.com/prod/janus';
 
 const SAMPLE_VERSION = false;
 
